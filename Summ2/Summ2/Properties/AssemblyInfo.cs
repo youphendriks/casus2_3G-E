e92af688b17b1b9db0b2ccea448c8bf1928075ba
[youphendriks] <[youp_hendriks@hotmail.com]>
2019-04-16 22:43:51 +0200

    Navbar
    Navbar+Background

--- Summ2/Summ2/Properties/AssemblyInfo.cs
+++ Summ2/Summ2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Summ2")]
+[assembly: AssemblyTitle("Casus2_3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Summ2")]
+[assembly: AssemblyProduct("Casus2_3")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
